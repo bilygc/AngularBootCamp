a26c304890c7ae6cc808d67b54a1c95d31388111
bilygc <licbgomez@gmail.com>
2024-05-07 20:46:08 -0700

    tarea de directivas estructurales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgForComponent } from './components/ng-for/ng-for.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgForComponent,
+    NgIfComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
